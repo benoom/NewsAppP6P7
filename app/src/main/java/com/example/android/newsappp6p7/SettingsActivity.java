ee3e8e8ee5ba8799170822b37c26e25e51c15562
Benjamin J. Oom <benjoom@gmail.com>
2018-10-27 13:17:13 -0700

    Create News, NewsActivity, NewsAdapter, NewsLoader, QueryUtils, and SettingsActivity java classes. Update AndroidManifest to include network permissions

--- app/src/main/java/com/example/android/newsappp6p7/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsappp6p7/SettingsActivity.java
@@ -3,7 +3,7 @@ package com.example.android.newsappp6p7;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
