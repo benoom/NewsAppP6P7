a094162747d52cafe8f594ce02567aad93c2cd58
Benjamin J. Oom <benjoom@gmail.com>
2018-11-02 12:50:46 -0700

    Initial commit of code for java and resource files

---
+++ app/src/main/java/com/example/android/newsappp6p7/News.java
@@ -0,0 +1,77 @@
+package com.example.android.newsappp6p7;
+
+/**
+ * An {@link News} object contains information related to a single soccer news story.
+ */
+public class News {
+
+    /** Title of the news article */
+    private String mArticleTitle;
+
+    /** Name of the section the article is categorized under */
+    private String mNameofSection;
+
+    /** Time of the article */
+    private String mTimeInMilliseconds;
+
+    /** Website URL of the news article */
+    private String mUrl;
+
+    /** Author or contributor for the news article */
+    private  String mContributor;
+
+    /**
+     * Constructs a new {@link News} object.
+     *
+     * @param articleTitle is the title of the news article
+     * @param nameOfSection is the section category that the article is categorized in
+     * @param timePublished is the date the news article was published
+     * @param url is the website URL to find more details about the news article
+     * @param contributor is the author/contributor of the news article
+     *
+     */
+    public News(String articleTitle, String nameOfSection, String timePublished, String url,
+                String contributor) {
+        mArticleTitle = articleTitle;
+        mNameofSection = nameOfSection;
+        mTimeInMilliseconds = timePublished;
+        mUrl = url;
+        mContributor = contributor;
+    }
+
+    /**
+     * Returns the title of the article.
+     */
+    public String getArticleTitle() {
+        return mArticleTitle;
+    }
+
+    /**
+     * Returns the section of the article.
+     */
+    public String getNameofSection() {
+        return mNameofSection;
+    }
+
+    /**
+     * Returns the publishing date of the article.
+     */
+    public String getTimePublished() {
+        return mTimeInMilliseconds;
+    }
+
+    /**
+     * Returns the website URL to find more information about the news article.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
+
+    /**
+     * Returns the contributor of the news article
+     */
+    public String getContributor(){
+        return mContributor;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/newsappp6p7/NewsAdapter.java
@@ -0,0 +1,95 @@
+package com.example.android.newsappp6p7;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * An {@link NewsAdapter} knows how to create a list item layout for each news article
+ * in the data source (a list of {@link News} objects).
+ *
+ * These list item layouts will be provided to an adapter view like ListView
+ * to be displayed to the user.
+ */
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    /**
+     * Constructs a new {@link NewsAdapter}.
+     *
+     * @param context of the app
+     * @param news is the list of news articles, which is the data source of the adapter
+     */
+    public NewsAdapter(Context context, List<News> news) {
+        super(context, 0, news);
+    }
+
+    /**
+     * Returns a list item view that displays information about the news article at the given
+     * position in the list of news articles.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if there is an existing list item view (called convertView) that we can reuse,
+        // otherwise, if convertView is null, then inflate a new list item layout.
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list_item, parent, false);
+        }
+
+        // Find the news article at the given position in the list of news articles
+        News currentNews = getItem(position);
+
+        // Find the TextView with view ID article_title
+        TextView titleView = listItemView.findViewById(R.id.article_title);
+        // Display the current news article in that TextView
+        titleView.setText(currentNews.getArticleTitle());
+
+        // Create a new Date object from the time in milliseconds of the news article
+        Date dateObject = new Date(currentNews.getTimePublished());
+
+        // Find the TextView with view ID date
+        TextView dateView = listItemView.findViewById(R.id.date);
+        // Format the date string (i.e. "Mar 3, 1984")
+        String formattedDate = formatDate(dateObject);
+        // Display the date of the current news article in that TextView
+        dateView.setText(formattedDate);
+
+        // Find the TextView with view ID time
+        TextView timeView = (TextView) listItemView.findViewById(R.id.time);
+        // Format the time string (i.e. "4:30PM")
+        String formattedTime = formatTime(dateObject);
+        // Display the time of the current news article in that TextView
+        timeView.setText(formattedTime);
+
+        // Return the list item view that is now showing the appropriate data
+        return listItemView;
+    }
+
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted time string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+}
